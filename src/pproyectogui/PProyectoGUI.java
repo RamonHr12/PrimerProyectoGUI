8e1513a9a51b8dac748f7ed4164669403d9aee95
Ramón Herrera Rodríguez <141000313@itcampeche.edu.mx>
2016-05-27 12:20:27 -0500

    Programa que suma y resta dos números

---
+++ src/pproyectogui/PProyectoGUI.java
@@ -0,0 +1,17 @@
+package pproyectogui;
+
+
+import gui.Bienvenido;
+
+/**
+ *
+ * @author ramon12
+ */
+public class PProyectoGUI {
+
+    public static void main(String[] args) {
+      Bienvenido windows = new Bienvenido();
+      windows.setVisible(true);
+    }
+    
+}
